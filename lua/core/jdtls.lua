e912b17c4ec2601468fa0e729bdc41894a582a13
KegsZooL <114305696+KegsZooL@users.noreply.github.com>
2024-06-21 17:55:14 +0300

    Update jdtls.lua

--- lua/core/jdtls.lua
+++ lua/core/jdtls.lua
@@ -40,9 +40,9 @@ end
 
 local function get_workspace()
     -- Get the home directory of your operating system
-    --local home = os.getenv "HOME"
+    local home = os.getenv "HOME"
     -- Declare a directory where you would like to store project information
-    --local workspace_path = home .. "/code/workspace/"
+    local workspace_path = home .. "/code/workspace/"
     local workspace_path = "C:/Users/KegsZooL/code/workspace"
     -- Determine the project name
     local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
