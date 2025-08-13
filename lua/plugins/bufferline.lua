1962acdcaf03c90ef269e65d962f589d20bffcab
KegsZooL <danil.chistyakov.2015@inbox.ru>
2025-08-13 20:39:39 +0500

    fix: change basedpyright config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,19 +1,3 @@
 local bufferline = require("bufferline")
 
-bufferline.setup {
-    options = {
-	    offsets = {
-		    {
-			    filetype = "NvimTree",
-				text = "File Tree",
-				highlight = "Directory",
-				separator = true, -- use a "true" to enable the default, or set your own character
-            },
-			},
-		diagnostics = "nvim_lsp",
-		separator_style = { "", "" },
-		modified_icon = "●",
-		show_close_icon = true,
-		show_buffer_close_icons = true,
-	},
-}
+bufferline.setup {}
