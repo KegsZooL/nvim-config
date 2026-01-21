676dbf888e208ed23caf704a4947da900ad2636a
KegsZooL <danil.chistyakov.2015@inbox.ru>
2026-01-21 13:30:52 +0500

    Edit mypy source definition for null ls

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,7 +1,6 @@
 local null_ls = require('null-ls')
 local util = require('lspconfig.util')
 
--- Custom code action: Implement abstract methods
 local implement_interface_action = {
     name = 'implement-interface',
     method = null_ls.methods.CODE_ACTION,
@@ -28,7 +27,6 @@ local implement_interface_action = {
             for _, node in query:iter_captures(root, bufnr) do
                 local start_row, _, end_row, _ = node:range()
                 if row >= start_row and row <= end_row then
-                    -- Сохраняем контекст для передачи в action
                     local saved_bufnr = bufnr
                     local saved_row = row
                     return {
@@ -48,48 +46,20 @@ local implement_interface_action = {
 null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.mypy.with({
-            condition = function(utils)
-                return utils.root_has_file({ "pyproject.toml", "mypy.ini", ".mypy.ini" })
-            end,
-            cwd = function(params)
-                return util.root_pattern("pyproject.toml", "mypy.ini", ".mypy.ini")(params.bufname)
-                    or vim.fn.getcwd()
-            end,
-            command = function(params)
-                local root_dir = util.root_pattern("pyproject.toml")(params.bufname)
-                if root_dir then
-                    -- Check .venv
-                    local venv_mypy = root_dir .. "/.venv/bin/mypy"
-                    if vim.fn.filereadable(venv_mypy) == 1 then
+            dynamic_command = function(params)
+                local root = util.root_pattern('pyproject.toml', 'setup.py', '.git')(params.bufname)
+                if root then
+                    local venv_mypy = root .. '/.venv/bin/mypy'
+                    if vim.fn.executable(venv_mypy) == 1 then
                         return venv_mypy
                     end
-                    -- Check pyenv virtualenv by project folder name
-                    local project_name = vim.fn.fnamemodify(root_dir, ":t")
-                    local pyenv_venv_mypy = vim.fn.expand("~/.pyenv/versions/" .. project_name .. "/bin/mypy")
-                    if vim.fn.filereadable(pyenv_venv_mypy) == 1 then
-                        return pyenv_venv_mypy
-                    end
-                    -- Check pyenv local version from .python-version
-                    local python_version_file = root_dir .. "/.python-version"
-                    if vim.fn.filereadable(python_version_file) == 1 then
-                        local version = vim.fn.readfile(python_version_file)[1]
-                        if version then
-                            local pyenv_mypy = vim.fn.expand("~/.pyenv/versions/" .. version .. "/bin/mypy")
-                            if vim.fn.filereadable(pyenv_mypy) == 1 then
-                                return pyenv_mypy
-                            end
-                        end
+
+                    venv_mypy = root .. '/venv/bin/mypy'
+                    if vim.fn.executable(venv_mypy) == 1 then
+                        return venv_mypy
                     end
                 end
-                return "mypy"
-            end,
-            extra_args = function(params)
-                local root_dir = util.root_pattern("pyproject.toml")(params.bufname)
-                if root_dir then
-                    return { "--config-file", root_dir .. "/pyproject.toml" }
-                else
-                    return {}
-                end
+                return 'mypy'
             end,
         }),
         implement_interface_action,
