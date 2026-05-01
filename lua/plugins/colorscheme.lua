6ce44d10e244dcca85adbe89770f532f10b454d9
KegsZooL <danil.chistyakov.2015@inbox.ru>
2026-05-01 09:22:47 +0500

    Change colorscheme with gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,6 @@
 return {
-  'savq/melange-nvim',
+  -- 'savq/melange-nvim',
+  'sainnhe/gruvbox-material',
   priority = 1000,
   config = function()
     local cmd = vim.cmd
@@ -7,15 +8,24 @@ return {
 
     vim.o.background = "dark"
 
-    cmd([[colorscheme melange]])
+    -- cmd([[colorscheme melange]])
+    -- Highlight groups for indent-blankline
+    -- api.nvim_set_hl(0, "IblScope", { fg = "#7daea3" })
+    -- api.nvim_set_hl(0, "IblIndent", { fg = "#3c3836" })
+
+    cmd("let g:gruvbox_material_background= 'medium'")
+    cmd("let g:gruvbox_material_transparent_background=1")
+    cmd("let g:gruvbox_material_diagnostic_line_highlight=1")
+    cmd("let g:gruvbox_material_diagnostic_virtual_text='colored'")
+    cmd("let g:gruvbox_material_enable_bold=1")
+    cmd("let g:gruvbox_material_enable_italic=1")
+
+    cmd([[colorscheme gruvbox-material]])
 
     api.nvim_set_hl(0, "FloatBorder", { link = "Normal" })
     api.nvim_set_hl(0, "LspInfoBorder", { link = "Normal" })
     api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
     api.nvim_set_hl(0, "IlluminatedWordRead", { fg = "#e78a4e", bold = true })
 
-    -- Highlight groups for indent-blankline
-    api.nvim_set_hl(0, "IblScope", { fg = "#7daea3" })
-    api.nvim_set_hl(0, "IblIndent", { fg = "#3c3836" })
   end
 }
